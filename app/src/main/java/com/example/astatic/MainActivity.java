8a07ba26fe22a1887b32468d0da2900291631a3d
marslankh092 <marslankh092@gmail.com>
2023-03-30 18:17:29 +0500

    Initial commit

---
+++ app/src/main/java/com/example/astatic/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.astatic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.audio_join_activity);
+    }
+}
